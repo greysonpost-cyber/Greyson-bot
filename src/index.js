e9e01a7dc65bd2b51fe10ce30345b475a6e0c44f
greysonpost-cyber <greysonpost@icloud.com>
2026-08-02 11:25:19 -0400

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const client = new Client({
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMembers,
         GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.DirectMessages,
         GatewayIntentBits.MessageContent,
         GatewayIntentBits.GuildVoiceStates,
         GatewayIntentBits.GuildModeration,
